3d3a8efa010ebd9bb36ea48d04d977a22bd886e5
Kristi <kylachka@mail.ru>
2023-01-10 10:54:49 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+string[] strArr = stroka.Split(' ');
+var result = new string[strArr.Length];
+int realSize = 0;
+foreach (var value in strArr)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(", ", result, 0, realSize));
